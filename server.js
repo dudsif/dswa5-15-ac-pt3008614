451f9b7e14cd4d5d6c77dade068967f1acdae4c4
leoouww <rcleonardo1995@gmail.com>
2022-05-05 20:43:17 -0300

    add integration with mongo

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.tdusj.mongodb.net:27017,cluster0-shard-00-01.tdusj.mongodb.net:27017,cluster0-shard-00-02.tdusj.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-vmzxmt-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
